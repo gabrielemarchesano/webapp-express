263d685bc546fda5b4f0ba0b18365e734141b052
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-12-10 14:06:30 +0100

    fix the order of the steps, add body parser

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// body parser
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 })
