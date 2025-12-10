d6a6640de12daab8488e53ddd800d73c3af06bbe
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-12-10 15:37:54 +0100

    move show function from router to the controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -13,8 +13,46 @@ function index(req, res){
     res.json(results);
     console.log(results);
   })
+};
+
+function show(req, res){
+  const id = req.params.id;
+  const sql = "SELECT * FROM movies WHERE id = ?";
+  const sqlReviews = "SELECT id, name, vote, text FROM reviews WHERE movie_id = ?";
+
+  connection.query(sql, [id], (err, results) => {
+    if(err){
+      return res.status(500).json({
+        error: "Db query failed"
+      });
+    };
+
+    if(results.length === 0){
+      return res.status(404).json({
+        error: true,
+        message: "Movie not found"
+      });
+    };
+
+    const movie = results[0];
+
+    connection.query(sqlReviews, [id], (errReviews, resultsReviews) => {
+      if(errReviews){
+        return res.status(500).json({
+        error: true,
+        message: errReviews.message
+        });
+      };
+
+      movie.reviews = resultsReviews;
+      res.json(movie);
+      console.log(movie);
+    });
+
+  })
 }
 
 module.exports = {
-  index
+  index,
+  show
 };
--- routes/movies.js
+++ routes/movies.js
@@ -1,48 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const connection = require("../database/connections");
+
 const movieController = require("../controllers/movieController");
 
 // index
 router.get("/", movieController.index);
 
 // show
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = "SELECT * FROM movies WHERE id = ?";
-  const sqlReviews = "SELECT id, name, vote, text FROM reviews WHERE movie_id = ?";
-
-  connection.query(sql, [id], (err, results) => {
-    if(err){
-      return res.status(500).json({
-        error: "Db query failed"
-      });
-    };
-
-    if(results.length === 0){
-      return res.status(404).json({
-        error: true,
-        message: "Movie not found"
-      });
-    };
-
-    const movie = results[0];
-
-    connection.query(sqlReviews, [id], (errReviews, resultsReviews) => {
-      if(errReviews){
-        return res.status(500).json({
-        error: true,
-        message: errReviews.message
-        });
-      };
-
-      movie.reviews = resultsReviews;
-      res.json(movie);
-      console.log(movie);
-    });
-
-  })
-
-})
+router.get("/:id", movieController.show);
 
 module.exports = router;
