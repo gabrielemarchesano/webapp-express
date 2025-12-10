1ad04c514b85f84a142868744ab58a729895dfdb
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-12-10 15:00:44 +0100

    add show route to get the movie by its id

--- routes/movies.js
+++ routes/movies.js
@@ -18,4 +18,31 @@ router.get("/", (req, res) => {
   })
 })
 
+// show
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM movies WHERE id = ?";
+
+  connection.query(sql, [id], (err, results) => {
+    if(err){
+      return res.status(500).json({
+        error: "Db query failed"
+      });
+    };
+
+    if(results.length === 0){
+      return res.status(404).json({
+        error: true,
+        message: "Book not found"
+      });
+    };
+
+    const book = results[0];
+    res.json(book);
+    console.log(book);
+
+  })
+
+})
+
 module.exports = router;
